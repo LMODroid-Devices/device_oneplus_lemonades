30017c45249b36e16d63f5c5a5da8b0a2f8f8852
LMO GerritBot <gerrit@libremobileos.com>
2024-04-19 19:31:51 +0000

    Adapt to LMODroid by device-add script

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lemonades.mk
+    $(LOCAL_DIR)/lmodroid_lemonades.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lemonades-user \
-    lineage_lemonades-userdebug \
-    lineage_lemonades-eng
+    lmodroid_lemonades-user \
+    lmodroid_lemonades-userdebug \
+    lmodroid_lemonades-eng
